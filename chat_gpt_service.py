7c1dba20cfcbe108efaabe3eccb0a0eb1aef9e29
YoavAmit20 <123245133+YoavAmit20@users.noreply.github.com>
2024-02-18 15:36:14 +0200

    hide_api_key

--- chat_gpt_service.py
+++ chat_gpt_service.py
@@ -1,39 +1,34 @@
 from openai import OpenAI
-import os
-
-os.environ['OPENAI_API_KEY'] = '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'
-client = OpenAI(
-    # This is the default and can omitted
-    api_key=os.environ.get("OPENAI_API_KEY"),
-    organization='org-2CwskBzgGP5OnJE5rJP2GrIS',
-)
+from dotenv import load_dotenv
+load_dotenv("config.env")
 
 
-class ChatGPTService:
+class ChatGPTService:  
 
-    def communicate_with_chatgpt(self, input_transcription):
-
-        input_transcription = str(input_transcription)
-        chat_completion = client.chat.completions.create(
+    def communicate_with_chatgpt(self, api_key, organization, input_transcription):
+            client = OpenAI(
+                api_key=api_key,
+                organization=organization,
+            )
+        
+            input_transcription = str(input_transcription)
+            chat_completion = client.chat.completions.create(
             messages=[
                 {
                     "role": "user",
                     "content": f"I am a doctor. Here is conversation between me and my patient: \n{input_transcription}\nWhat alternative tests are recommended to the patient to do? list shortly only 3 tests. Don't write anything else, just the 3 tests. Please also explain in one sentence each of the 3 tests"
                 }
             ],
-            model="gpt-3.5-turbo",
-            stream=True,
+            model="gpt-3.5-turbo", 
+            stream=True,       
         )
-        result = ""
-        for chunk in chat_completion:
-            if chunk.choices[0].delta.content is not None:
-                result += chunk.choices[0].delta.content
-        return result
+            result = ""
+            for chunk in chat_completion:
+                if chunk.choices[0].delta.content is not None:
+                    result += chunk.choices[0].delta.content  
+            return result
+    
 
-    def generate_embeddings(self, input_transcription, topic):
 
-        response = client.embeddings.create(
-            input=topic + "\n" + input_transcription,
-            model="text-embedding-ada-002"
-        )
-        return response.data[0].embedding
+
+    
